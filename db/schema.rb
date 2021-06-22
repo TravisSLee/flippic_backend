93628d8bba2394cf6d27d88c6e0b41dd1ce96b5d
TravisSLee <travisslee223@gmail.com>
2021-06-22 15:20:50 -0400

    add posts mvc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_09_173521) do
+ActiveRecord::Schema.define(version: 2021_06_22_191349) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
